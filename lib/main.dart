de744fc857e9a8b53ce1bc68da1005756f0e817a
Max Herz <maxopie@gmail.com>
2019-06-09 13:54:18 -0400

    File Organazation/Maintainence

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 
+import './product_manger.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: Column(children: [
-            Container(
-              margin: (EdgeInsets.all(10.0)),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advanced Food Tester');
-                  });
-                },
-                child: Text('Add more'),
-              ),
-            ),
-            Column(
-              children: _products
-                  .map(
-                    (element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assests/pic.png'),
-                              Text(element)
-                            ],
-                          ),
-                        ),
-                  )
-                  .toList(),
-            )
-          ])),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManger(),
+      ),
     );
   }
 }
