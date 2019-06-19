973f4fdbe73185dc874ec85565e95db65e6c6688
Max Herz <maxopie@gmail.com>
2019-06-18 22:21:32 -0400

    Conditional Rendering + ListView

--- lib/products.dart
+++ lib/products.dart
@@ -4,21 +4,32 @@ class Products extends StatelessWidget {
   final List<String> products;
   Products(this.products);
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assests/pic.png'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCard;
+    if (products.length == 0) {
+      productCard = Center(child: Text("No Products Found"));
+    } else {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    }
+    return productCard;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: products
-          .map(
-            (element) => Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assests/pic.png'),
-                      Text(element)
-                    ],
-                  ),
-                ),
-          )
-          .toList(),
-    );
+    return _buildProductList();
   }
 }
